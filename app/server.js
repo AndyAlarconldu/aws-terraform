eb271a9a633770e373e2a1707d8b2dcf47034d55
AndyAlarconldu <andyjavier301067@gmail.com>
2025-12-15 20:36:29 -0500

    add server,js

--- app/server.js
+++ app/server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir el frontend estático
-app.use(express.static(path.join(__dirname, "public")));
+// Servir el index.html que está en la carpeta /app
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-// Endpoint backend
+// Backend API
 app.get("/api/hello", (req, res) => {
   res.json({
-    message: "Hola desde el backend en AWS con Auto Scaling - Andy Alarcón"
+    message: "Hola desde el backend ✅ (Terraform + AWS + ALB + ASG) - Andy Alarcón"
   });
 });
 
-// Cualquier otra ruta => index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Health check (para AWS ALB)
+app.get("/health", (req, res) => res.status(200).send("OK"));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
 });
+
 
