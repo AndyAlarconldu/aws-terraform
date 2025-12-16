1f7ae2e5a1ec5b04087eb3d056f687332dce4017
AndyAlarconldu <andyjavier301067@gmail.com>
2025-12-15 20:24:32 -0500

    add

--- app/server.js
+++ app/server.js
@@ -18,6 +18,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
 });
+
